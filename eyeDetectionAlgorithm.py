327a3b8fbfd3c29a25b7f9a520bb9414f325d56a
Tharaka Ratnayake <tharakasachintha27@gmail.com>
2021-12-11 15:15:26 +0530

    eye retina detection complete

--- eyeDetectionAlgorithm.py
+++ eyeDetectionAlgorithm.py
@@ -1,15 +1,153 @@
-from picamera import PiCamera
+
+'''
+#from picamera import PiCamera
 from time import sleep
 import math
 import cv2
 import numpy as np
 
-camera = PiCamera()
-camera.resolution = (1920,1080)
-camera.start_preview()
-camera.capture('/home/pi/Desktop/img.jpg')
-sleep(5)
-camera.stop_preview()
+#camera = PiCamera()
+#camera.resolution = (1920,1080)
+#camera.start_preview()
+#camera.capture('/home/pi/Desktop/img.jpg')
+#sleep(5)
+#camera.stop_preview()
 
-img = cv2.imread('/home/pi/Desktop/img.jpg')
+#img = cv2.imread('/home/pi/Desktop/img.jpg')
+#scaling_factor = 0.7
+
+cam = cv2.VideoCapture(0)
+
+cv2.namedWindow("test")
 scaling_factor = 0.7
+img_counter = 0
+
+while True:
+    ret, frame = cam.read()
+    if not ret:
+        print("failed to grab frame")
+        break
+    #cv2.imshow("test", frame)
+    gray = cv2.cvtColor(~frame, cv2.COLOR_BGR2GRAY)
+
+    ret, thresh_gray = cv2.threshold(gray, 220, 255, cv2.THRESH_BINARY)
+    contours, hierarchy = cv2.findContours(thresh_gray, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_NONE)
+
+    for contour in contours:
+        area = cv2.contourArea(contour)
+        rect = cv2.boundingRect(contour)
+        x, y, width, height = rect
+        radius = 0.25 * (width + height)
+        
+        area_condition = (100 <= area <= 200)
+        symmetry_condition = (abs(1 - float(width)/float(height)) <= 0.2)
+        fill_condition = (abs(1 - (area / (math.pi * math.pow(radius, 2.0)))) <= 0.3)
+        
+        if area_condition and symmetry_condition and fill_condition:
+            cv2.circle(frame, (int(x + radius), int(y + radius)), int(1.3*radius), (0,180,0), -1)
+    
+    cv2.imshow('Pupil Detector', frame)
+
+    k = cv2.waitKey(1)
+    if k%256 == 27:
+        # ESC pressed
+        print("Escape hit, closing...")
+        break
+    elif k%256 == 32:
+        # SPACE pressed
+        img_name = "opencv_frame_{}.png".format(img_counter)
+        cv2.imwrite(img_name, frame)
+        print("{} written!".format(img_name))
+        img_counter += 1
+
+cam.release()
+
+cv2.destroyAllWindows()
+'''
+
+import cv2
+import dlib
+import numpy as np
+
+def shape_to_np(shape, dtype="int"):
+	# initialize the list of (x, y)-coordinates
+	coords = np.zeros((68, 2), dtype=dtype)
+	# loop over the 68 facial landmarks and convert them
+	# to a 2-tuple of (x, y)-coordinates
+	for i in range(0, 68):
+		coords[i] = (shape.part(i).x, shape.part(i).y)
+	# return the list of (x, y)-coordinates
+	return coords
+
+def eye_on_mask(mask, side):
+    points = [shape[i] for i in side]
+    points = np.array(points, dtype=np.int32)
+    mask = cv2.fillConvexPoly(mask, points, 255)
+    return mask
+
+def contouring(thresh, mid, img, right=False):
+    cnts, _ = cv2.findContours(thresh, cv2.RETR_EXTERNAL,cv2.CHAIN_APPROX_NONE)
+    try:
+        cnt = max(cnts, key = cv2.contourArea)
+        M = cv2.moments(cnt)
+        cx = int(M['m10']/M['m00'])
+        cy = int(M['m01']/M['m00'])
+        if right:
+            cx += mid
+        cv2.circle(img, (cx, cy), 4, (0, 0, 255), 2)
+    except:
+        pass
+
+detector = dlib.get_frontal_face_detector()
+#predictor = dlib.shape_predictor('shape_68.dat') shape_predictor_68_face_landmarks
+predictor = dlib.shape_predictor('shape_predictor_68_face_landmarks.dat')
+left = [36, 37, 38, 39, 40, 41]
+right = [42, 43, 44, 45, 46, 47]
+
+cap = cv2.VideoCapture(0)
+ret, img = cap.read()
+thresh = img.copy()
+
+cv2.namedWindow('image')
+kernel = np.ones((9, 9), np.uint8)
+
+def nothing(x):
+    pass
+    
+cv2.createTrackbar('threshold', 'image', 0, 255, nothing)
+
+while(True):
+    ret, img = cap.read()
+    gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+    rects = detector(gray, 1)
+    for rect in rects:
+
+        shape = predictor(gray, rect)
+        shape = shape_to_np(shape)
+        mask = np.zeros(img.shape[:2], dtype=np.uint8)
+        mask = eye_on_mask(mask, left)
+        mask = eye_on_mask(mask, right)
+        mask = cv2.dilate(mask, kernel, 5)
+        eyes = cv2.bitwise_and(img, img, mask=mask)
+        mask = (eyes == [0, 0, 0]).all(axis=2)
+        eyes[mask] = [255, 255, 255]
+        mid = (shape[42][0] + shape[39][0]) // 2
+        eyes_gray = cv2.cvtColor(eyes, cv2.COLOR_BGR2GRAY)
+        threshold = cv2.getTrackbarPos('threshold', 'image')
+        _, thresh = cv2.threshold(eyes_gray, threshold, 255, cv2.THRESH_BINARY)
+        thresh = cv2.erode(thresh, None, iterations=2) #1
+        thresh = cv2.dilate(thresh, None, iterations=4) #2
+        thresh = cv2.medianBlur(thresh, 3) #3
+        thresh = cv2.bitwise_not(thresh)
+        contouring(thresh[:, 0:mid], mid, img)
+        contouring(thresh[:, mid:], mid, img, True)
+        # for (x, y) in shape[36:48]:
+        #     cv2.circle(img, (x, y), 2, (255, 0, 0), -1)
+    # show the image with the face detections + facial landmarks
+    cv2.imshow('eyes', img)
+    cv2.imshow("image", thresh)
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+        break
+    
+cap.release()
+cv2.destroyAllWindows()
